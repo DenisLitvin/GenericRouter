9c1696f09665b07bf3d3cc2be9fc73fac9638e61
Denis Litvin <den.litvinn@gmail.com>
2018-08-08 11:51:31 +0300

    Initial Commit

---
+++ Router/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Router
+//
+//  Created by Denis Litvin on 08.08.2018.
+//  Copyright © 2018 Denis Litvin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
